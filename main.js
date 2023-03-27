929395215dddcc971ab2cd402a64416383ca4d95
Murad Gojayev <muradqocayevi20@gmail.com>
2023-03-28 01:16:40 +0400

    2

--- main.js
+++ main.js
@@ -5,8 +5,7 @@
 // Input: nums = [3,2,4], target = 6
 // Output: [1,2]
 
-let nums = [3, 2, 4, 1, 5],
-    target = 6;
+
 
 function twoSum(nums, target) {
 
@@ -23,4 +22,38 @@ function twoSum(nums, target) {
 
     return returnArr
 }
+
+
+//9. Palindrome Number => easy 
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads - 121. From right to left, it becomes 121 -.Therefore it is not a palindrome.
+// Could you solve it without converting the integer to a string?
+
+function checkPalindrome(num) {
+    if (num < 0 || num % 1 != 0) return false
+
+    let valuableNum = num
+    let reverseNum = 0
+
+    while (true) {
+
+        reverseNum = (num % 10) + reverseNum * 10
+        num = (num / 10 - num % 10 / 10).toFixed()
+
+        if (num < 1) {
+            break
+        }
+
+    }
+
+    if (reverseNum == valuableNum) {
+        return true
+    }
+
+
+    return false
+
+}
+
 
